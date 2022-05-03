0d125943882a078808ee88d5d2c07acba465d5c5
tejas falke <tejasf4@gmail.com>
2022-05-03 09:39:07 +0530

    Create add_two_number_by_user.py
    I am going to start Algorithm using python ..it is a simple but it is important..

---
+++ add_two_number_by_user.py
@@ -0,0 +1,15 @@
+"""
+step 1: Start
+step 2: Declare variables num1, num2 and sum
+step 3: Read the values num1, num2 and sum
+step 4: Add num1 and num2 and assign the result to sum
+        sum<-num1+num2
+step 5: Display sum
+step 6: Stop
+"""
+
+num1 = int(input("Enter num1 :"))
+num2 = int(input("Enter num2 :"))
+
+sum=num1+num2
+print("Sum",sum)
